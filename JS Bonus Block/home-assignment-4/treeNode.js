83cd2d5e30b46b5683109b6cdea3a688858672d8
Yurii Kozlov <yurka22870@gmail.com>
2025-05-14 00:12:45 +0300

    feat: implement JS Bonus block HA 4

---
+++ JS Bonus Block/home-assignment-4/treeNode.js
@@ -0,0 +1,25 @@
+class TreeNode {
+  children = [];
+
+  constructor(value) {
+      this.value = value;
+  }
+
+
+  addChild(childValue) {
+      const childNode = new TreeNode(childValue);
+      this.children.push(childNode);
+
+      return childNode;
+  }
+}
+
+// use cases
+const root = new TreeNode('Root');
+const child1 = root.addChild('Child 1');
+const child2 = root.addChild('Child 2');
+child1.addChild('Grandchild 1.1');
+child1.addChild('Grandchild 1.2');
+child2.addChild('Grandchild 2.1');
+
+console.log(JSON.stringify(root, null, 2));
